678ad036b9385b2bbef90c2e405dd89541b1c4cc
cipeng <fmulkhan@gmail.com>
2025-12-24 00:06:01 +0800

    fix: Allow domain in vite config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '127.0.0.1',
+    port: 4173,
+    allowedHosts: [
+      'besopok.com',
+      'www.besopok.com'
+    ]
+  }
 })
